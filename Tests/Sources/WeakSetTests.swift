c7cbc361bff692e0fc3487135b16fe21c64c3572
Frederick Pietschmann <frederick.pietschmann@web.de>
2018-03-13 22:59:41 +0100

    Add weak set tests

--- Tests/Sources/WeakSetTests.swift
+++ Tests/Sources/WeakSetTests.swift
@@ -10,8 +10,54 @@
 import XCTest
 
 class WeakSetTests: XCTestCase {
-    // TODO: Test properly
-    func test() {
-        XCTAssertEqual(1, 1)
+    // MARK: - Subtypes
+    class SampleObject: Hashable {
+        var hashValue: Int
+
+        init(hashValue: Int) {
+            self.hashValue = hashValue
+        }
+
+        static func == (lhs: WeakSetTests.SampleObject, rhs: WeakSetTests.SampleObject) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
+    }
+
+    // MARK: - Properties
+    private let setSize = 3
+    private lazy var set: Set<SampleObject>? = {
+        Set((0..<setSize).map { index in SampleObject(hashValue: index) })
+    }()
+
+    // MARK: - Methods
+    func testCount() {
+        let weakSet = WeakSet(set!)
+        XCTAssertEqual(weakSet.contents.count, set!.count)
+    }
+
+    func testIsEmpty() {
+        let weakSet = WeakSet(set!)
+        XCTAssertEqual(weakSet.isEmpty, set!.isEmpty)
+    }
+
+    func testInsertingAndRemoving() {
+        var weakSet = WeakSet(set!)
+
+        // Test inserting
+        let sample = set!.first!
+        let sampleCount = 3
+        for _ in 0..<sampleCount { weakSet.insert(sample) }
+        XCTAssertEqual(weakSet.contents.filter { $0 === sample }.count, 1)
+
+        // Test removing
+        weakSet.remove(sample)
+        XCTAssertEqual(weakSet.contents.count, setSize - 1)
+    }
+
+    func testCleaning() {
+        let weakSet = WeakSet(set!)
+        set = nil
+        XCTAssertEqual(weakSet.contents.count, 0)
+        XCTAssertEqual(weakSet.isEmpty, true)
     }
 }
