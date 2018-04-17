f034329040c292b1f04118b2302692bd90f1532a
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2018-04-17 21:43:56 +0200

    Add WeakDictionary tests

--- Tests/Sources/WeakDictionaryTests.swift
+++ Tests/Sources/WeakDictionaryTests.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+@testable import SwiftCollections
+import XCTest
+
+class WeakDictionaryTests: XCTestCase {
+    // MARK: - Subtypes
+    class SampleObject: Hashable {
+        var hashValue: Int
+
+        init(_ hashValue: Int) {
+            self.hashValue = hashValue
+        }
+
+        static func == (lhs: SampleObject, rhs: SampleObject) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
+    }
+
+    // MARK: - Properties
+    private let dictionarySize = 3
+    private lazy var dictionary: [Int: SampleObject]? = {
+        let tuples = (0..<dictionarySize).map { (key: $0, value: SampleObject($0)) }
+        return Dictionary(tuples) { _, _ in fatalError() }
+    }()
+
+    // MARK: - Methods
+    func testCount() {
+        let weakDictionary = WeakDictionary(dictionary!)
+        XCTAssertEqual(weakDictionary.contents.count, dictionary!.count)
+    }
+
+    func testAddingAndRemoving() {
+        var weakDictionary = WeakDictionary(dictionary!)
+
+        // Test adding
+        let sampleKey = dictionary!.count
+        let sampleValue = dictionary!.first!.value
+        weakDictionary[sampleKey] = sampleValue
+        XCTAssertEqual(weakDictionary[sampleKey], sampleValue)
+        XCTAssertEqual(weakDictionary.contents.values.count, dictionary!.count + 1)
+
+        // Test removing
+        weakDictionary[sampleKey] = nil
+        XCTAssertEqual(weakDictionary[sampleKey], nil)
+        XCTAssertEqual(weakDictionary.contents.values.count, dictionary!.count)
+    }
+
+    func testCleaning() {
+        let weakDictionary = WeakDictionary(dictionary!)
+        dictionary = nil
+        XCTAssertEqual(weakDictionary.contents.isEmpty, true)
+    }
+}
